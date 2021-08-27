9dc64c1a333a6e882512f7c65bdba598e4030de2
Alan Nonato da Silva <alan_nonato.silva@hotmail.com>
2021-08-27 01:25:47 -0300

    Rotas

--- resources/views/app/order/index.blade.php
+++ resources/views/app/order/index.blade.php
@@ -1,6 +1,6 @@
 @extends('app.layouts.app')
 @section('content')
-
+<h1 class="primary">Pedidos</h1>
 @endsection
 
 @section('script')
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('/', "App\Http\Controllers\HomeController");
+Route::resource('/checkout', "App\Http\Controllers\OrderController");
+Route::get('/cozinha', "App\Http\Controllers\KitchenController@index");
